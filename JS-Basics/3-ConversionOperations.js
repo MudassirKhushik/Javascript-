d2e834023e0fd7461a4b69fc67ae4b3f74b4a325
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-03-09 15:15:51 +0000

    Conversion of Operations

--- JS-Basics/3-ConversionOperations.js
+++ JS-Basics/3-ConversionOperations.js
@@ -1,6 +1,12 @@
 let score = 33
 console.log(typeof score) // number
-let score = "33"
-console.log(typeof(score)) // String
-let ValueInNumber = Number(score)
+
+let Score = "33"
+console.log(typeof(Score)) // String
+
+let ValueInNumber = Number(Score) // String to Number
 console.log(ValueInNumber);
+
+let value = "33abc"
+console.log(typeof value); // String
+console.log(value); // NaN
