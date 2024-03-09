cb9d9ac39d4f3cbb466b17be8f0f14d14577749b
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-03-09 14:55:59 +0000

    Datatypes

--- JS-Basics/2-Datatypes.js
+++ JS-Basics/2-Datatypes.js
@@ -4,3 +4,13 @@ let name = "Mudassir"
 let age = 19
 
 // Datatypes
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
