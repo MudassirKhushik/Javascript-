b705d19be0a3b4124e59a0cb0b903fe007a265a5
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-03-09 15:36:22 +0000

    Conversion of Operations

--- JS-Basics/3-ConversionOperations.js
+++ JS-Basics/3-ConversionOperations.js
@@ -7,6 +7,31 @@ console.log(typeof(Score)) // String
 let ValueInNumber = Number(Score) // String to Number
 console.log(ValueInNumber);
 
-let value = "33abc"
+let _score = "33abc"
+let value = Number(_score)
 console.log(typeof value); // String
 console.log(value); // NaN
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN 
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+let boolean = Boolean(isLoggedIn)
+console.log(boolean); // true
+
+let LoggedIn = ""
+let Boolean = Boolean(LoggedIn)
+console.log(Boolean); // false
+
+let Logged = "mudassir"
+let Bool = Boolean(Logged)
+console.log(Bool); // true
+
+// 1 => true
+// 0 => false
+// "" => false
+// "anytext" => true
