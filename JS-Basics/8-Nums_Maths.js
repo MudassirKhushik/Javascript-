49084162a9a7f755d9ce980a3ef8f493474831b5
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-03-22 15:25:49 +0000

    Number Properties

---
+++ JS-Basics/8-Nums_Maths.js
@@ -0,0 +1,14 @@
+const score=100
+console.log(score);
+
+const balance=new Number(400)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1)); // Numbers after decimal 
+
+const anyNum=23.8966
+console.log(anyNum.toPrecision(3)); // Roundoff 3 digits
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString());
