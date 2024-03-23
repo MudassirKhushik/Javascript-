2e2bc21adc244b78de37622ff38684da2dff8ce8
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-03-23 08:23:38 +0000

    Numbers and Maths in JS

--- JS-Basics/8-Nums_Maths.js
+++ JS-Basics/8-Nums_Maths.js
@@ -12,3 +12,20 @@ console.log(anyNum.toPrecision(3)); // Roundoff 3 digits
 
 const hundreds=1000000
 console.log(hundreds.toLocaleString());
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+
+// +++++++++++++++++ Maths +++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4)); // converts negative value to positive
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.min(2, 3, 7, 6));
+console.log(Math.max(2, 3, 7, 6));
+
+console.log(Math.random()); // gives the value b/w 0 and 1
+console.log((Math.random() * 10)+ 1);
+console.log(Math.floor((Math.random() * 10)+ 1));
