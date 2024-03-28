d4ff7caf2027ffd74b722eced030db9d968452e6
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-03-28 07:32:28 +0000

    Datatypes in JS

---
+++ 01-JS_Basics/2-Datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // Treat all JS code as newer version.
+
+let name = "Mudassir"
+let age = 19
+
+// Datatypes
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
