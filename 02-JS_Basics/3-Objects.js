910a9e7b57669f57595b527dadccf051af254a04
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-03-29 13:37:19 +0000

    JS Objects

---
+++ 02-JS_Basics/3-Objects.js
@@ -0,0 +1,16 @@
+// singleton
+// object.create
+
+// Object Literals
+
+const mySym = Symbol("Key1")
+
+const JSuser = {
+    name: "Mudassir",
+    "full name": "Mudassir Khushik",
+    [mySym]:"key1", //Symbol can be put in object
+    age: 19,
+    location: "Jamshoro",
+    email: "Khushik@gmail.com",
+
+} 
