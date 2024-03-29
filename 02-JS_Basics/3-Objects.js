6ebe0a9a0ea970d4cff6d53b017cca9314dbf2ff
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-03-29 15:22:52 +0000

    JS Objects

--- 02-JS_Basics/3-Objects.js
+++ 02-JS_Basics/3-Objects.js
@@ -11,5 +11,10 @@ const JSuser = {
     age: 19,
     location: "Jamshoro",
     email: "Khushik@gmail.com",
-
+    isLoggedIn: false,
+    LoginDays: ["Monday","Wednesday","Friday"]
 } 
+console.log(JSuser.name);
+console.log(JSuser["email"]);
+console.log(JSuser["full name"]);
+console.log(JSuser[mySym]);
