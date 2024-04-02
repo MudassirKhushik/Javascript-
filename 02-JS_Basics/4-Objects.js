e4dc3f630048ca7934f6cf446bc8ad0b0005904e
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-04-02 14:39:59 +0000

    More about Objects

---
+++ 02-JS_Basics/4-Objects.js
@@ -0,0 +1,10 @@
+// Singleton Object 
+// const tinderUser = new Object{}
+
+// Object Literals
+const tinderUser = {}
+tinderUser.id="123abc"
+tinderUser.name="Ahmed"
+tinderUser.email="mudassir@gmail.com"
+
+console.log(tinderUser);
