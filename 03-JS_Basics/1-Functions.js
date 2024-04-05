bd71c09ba79adf3a99e9a66bee0273d08ef53283
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-04-05 15:30:17 +0000

    Functions in JS

---
+++ 03-JS_Basics/1-Functions.js
@@ -0,0 +1,12 @@
+function MyName(){
+    console.log("M");
+    console.log("U");
+    console.log("D");
+    console.log("A");
+    console.log("S");
+    console.log("S");
+    console.log("I");
+    console.log("R");
+}
+
+MyName()
