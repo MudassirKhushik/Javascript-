d9df4411f58e093de593b6cc8f4d54f519d62974
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-04-05 15:21:46 +0000

    Objects and APIs in JS

--- 02-JS_Basics/5-Objects_APIs.js
+++ 02-JS_Basics/5-Objects_APIs.js
@@ -9,3 +9,11 @@ console.log(course.courseInstructor);
 const {courseInstructor: instructor} = course
 // console.log(courseInstructor);
 console.log(instructor);
+
+// JSON file structure
+// It can be in object as well as in array form
+// {
+//     "name":"Mudassir Khushik",
+//     "coursename":"Javascript",
+//     "price":"free"
+// }
