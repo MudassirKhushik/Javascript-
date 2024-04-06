775edcc48ddb1837b72e3ff42fb1606f7e8a010c
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-04-06 08:03:23 +0000

    Scope in JS

---
+++ 03-JS_Basics/2-Scope.js
@@ -0,0 +1,14 @@
+// var c=300
+if (true) {
+    let a=10
+    const b=20
+    var c=30    
+    console.log(a);
+    function add(num1,num2){
+        sum=num1+num2
+    }
+}
+
+// console.log(a); // These can't be accessd outside their scope 
+// console.log(b);
+// console.log(c); // This can be accessed and mostly causes errors 
