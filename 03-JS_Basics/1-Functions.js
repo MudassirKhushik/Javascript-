b19782a6e2956f5b7f4b09ba8ea8ff75fcd2a2ad
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-04-06 06:46:23 +0000

    Functions in detail

--- 03-JS_Basics/1-Functions.js
+++ 03-JS_Basics/1-Functions.js
@@ -24,4 +24,19 @@ function subtract(number1, number2){
     console.log("Mudassir"); // nothing is printed after return
 }
 
-subtract(15, 5)
+console.log(subtract(15, 5));
+
+function UserLoginMessage(username = "User"){
+    return `${username} just logged in`
+}
+console.log(UserLoginMessage("Mudassir"));
+
+function CalculateItems(...num1){ // rest Operator(used to add undefined arguments in an Array)
+    return num1
+}
+CalculateItems(200, 300, 450, 8000)
+
+const user = {
+    username:"Mudassir",
+    pice:299
+}
