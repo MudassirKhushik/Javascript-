378a94b501dee27526d43c729b107e44ea07f179
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-04-07 12:58:01 +0000

    Scope in JS

--- 03-JS_Basics/2-Scope.js
+++ 03-JS_Basics/2-Scope.js
@@ -12,3 +12,14 @@ if (true) {
 // console.log(a); // These can't be accessd outside their scope 
 // console.log(b);
 // console.log(c); // This can be accessed and mostly causes errors 
+
+function one(){
+    const username="Mudassir"
+    function Two(){
+        const channel="YouTube Channel"
+        console.log(username); // Inner function can access the attributes of outer ones
+    }
+    // console.log(channel); // can't be accessed outside the scope 
+    Two()
+}
+one()
