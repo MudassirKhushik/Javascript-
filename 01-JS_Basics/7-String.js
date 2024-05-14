6891f596b8ba3d8f78989979dd7d248b78d48f1b
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-13 21:49:35 -0700

    String in JS

--- 01-JS_Basics/7-String.js
+++ 01-JS_Basics/7-String.js
@@ -13,7 +13,8 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('a'));
 
-const newString=gameName.substring(0, 4) // Starting point to one less ythan ending point.
+const newString=gameName.substring(0, 4) // Starting point to one less than ending point.
+// substring method ignores the negative numbers and starts from 0.  
 console.log(newString);
 
 const anotherString=gameName.slice(-8, 3) // Negavitive Numbers can be included
@@ -21,7 +22,7 @@ console.log(anotherString);
 
 const newStringOne="    Mudassir    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim()); // Ths method ignores the blank spaces before and sfter the text.
 
 const url="https://mudassirkhushik.com/Mudassir%20Khushik"
 console.log(url.replace("%20", "-"));
