fbd25c67d2b0e960d4dbf000df34240bc816437e
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-15 04:59:40 +0000

    Scopes in JS

--- 03-JS_Basics/2-Scope.js
+++ 03-JS_Basics/2-Scope.js
@@ -25,3 +25,27 @@ function one(){
 }
 one()
 // The inner function can access the attributes of outer function but the outer one can't access the inner ones attributes. 
+
+// Setting Scope in If-Else
+if(true){
+    const user1name="Mudassir Khushik"
+    if(user1name === "Mudassir Khushik"){
+        const course = " Learning Javascript"
+        console.log(user1name + course);
+    }
+    // console.log(course); // Can't be accessed because of block scope
+}
+// console.log(user1name); // Can't be accessed because of block scope
+
+// +++++++++++++++++++++++ Hoisting +++++++++++++++++++++++
+console.log(addOne(5));
+// The function which is not stored in any variable can be called before decleration
+function addOne(num) {
+    return num + 1    
+} 
+
+// console.log(addTwo(5));// The function which is stored in any variable, Can't be called before decleration
+const addTwo = function(Num){
+    return Num + 2
+}
+console.log(addTwo(5));
