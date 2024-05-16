8020ebe5843cc7a55c5c2d8094746fa3c9a6375b
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-16 14:08:34 +0000

    For of Loop in JS

---
+++ 05-JS_Iterations/3-For_of_Loop.js
@@ -0,0 +1,30 @@
+// For of Loop
+// ["","",""]
+// [{},{},{}]
+
+let myArr = [1,2,3,4,5]
+for (const nums of myArr) {
+    console.log(nums);
+}
+
+let fullName = "Mudassir Khsuhik"
+for (const name of fullName) {
+    console.log(name);
+}
+
+// Maps
+// Prints the keys and values in a particular order and holds distinct elements(no repetition)
+
+const map = new Map()
+map.set("PKR" , "Paksitan Rupees")
+map.set("USD" , "United States Dollar")
+map.set("KD" , "Kuwait Dinar")
+map.set("PKR" , "Pakistan Rupees")
+
+// console.log(map); // The map is printed
+for (const [keys , values] of map) {
+    console.log(keys , "=" , values);   
+}
+// for of loop prints the values in array, but `[var1 , var2]` returns the values(no Array)
+
+// For of Loop is not valid for objects
