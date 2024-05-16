f2f26da2218d53d4564be21473c72b70aec7ebd8
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-16 13:43:53 +0000

    While do-while Loop

---
+++ 05-JS_Iterations/2-While_do_Loop.js
@@ -0,0 +1,21 @@
+// While Loop
+let count = 0
+while (count <= 10) {
+    console.log(count);
+    count+=2 // count = count+2
+}
+
+let myArray = ["Uzair", "Mahrus", "Mudassir"]
+let arr = 0
+while(arr < myArray.length){
+    console.log(myArray[arr]);
+    arr++
+}
+
+// do-while Loop
+let score = 11
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10);
+// Even if the condition doesn't match, the statement will be printed once
