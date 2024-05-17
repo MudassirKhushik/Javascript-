5fae9766a0d4d062c7f45e45dbbfebc0ccfaa8b6
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-17 06:19:41 +0000

    For-each Loop in JS

---
+++ 05-JS_Iterations/5-For_each_Loop.js
@@ -0,0 +1,22 @@
+// For-Each Loop
+const Arr = ["Pakistan", "India", "Nepal", "Japan", "Bangladesh"]
+// Arr.forEach( function (val) {
+//     console.log(val);
+// })
+
+// Arr.forEach( (items) => {
+//     console.log(items);
+// })
+
+// function printArr(vals){
+//     console.log(vals);
+// }
+// Arr.forEach(printArr)
+
+// Arr.forEach( (items, index, arr) => {
+//     console.log(items , index, arr);
+// })
+
+// Arr.forEach(element => {
+//     console.log(element);
+// });
