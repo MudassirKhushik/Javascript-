43ee3f527d722b90e061af79a834b1637d1c58f2
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-17 05:40:27 +0000

    For-in Loop in JS

--- 05-JS_Iterations/4-For_in_Loop.js
+++ 05-JS_Iterations/4-For_in_Loop.js
@@ -11,5 +11,18 @@ for (const keys in myObj) {
 
 const myArray = ["py", "cpp", "java", "rb", "js"]
 for (const key in myArray) {
-    console.log(key);
+    // console.log(key); // It will only give keys
+    console.log(myArray[key]);
 }
+
+// Maps
+// Prints the keys and values in a particular order and holds distinct elements(no repetition).
+// const map = new Map()
+// map.set("PKR" , "Paksitan Rupees")
+// map.set("USD" , "United States Dollar")
+// map.set("KD" , "Kuwait Dinar")
+// map.set("PKR" , "Pakistan Rupees")
+// for (const key in map){
+//     console.log(key);
+// }
+// The map is not iterable
