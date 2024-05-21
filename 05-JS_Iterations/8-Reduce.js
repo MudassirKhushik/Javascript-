e08a99b36cd8ec33fd489da669c6c74af3b1380d
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-21 15:56:06 +0000

    Reduce method in JS

--- 05-JS_Iterations/8-Reduce.js
+++ 05-JS_Iterations/8-Reduce.js
@@ -1,6 +1,9 @@
 // Reduce method 
 const myArray = [1,2,3,4,5]
-const total = myArray.reduce( function (acc, curr) { 
+let total = myArray.reduce( function (acc, curr) { 
     return acc + curr
 },0)
+console.log(total);
+
+total = myArray.reduce( (acc,curr) => acc + curr,0)
 console.log(total);
