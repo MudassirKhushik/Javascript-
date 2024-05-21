7e03c2dac1f0c5ba366b9ec4a8999a12bbb67ee5
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-21 15:27:16 +0000

    Reduce method in JS

--- 05-JS_Iterations/7-Map.js
+++ 05-JS_Iterations/7-Map.js
@@ -1,3 +1,9 @@
 const myNums = [1,2,3,4,5,6,7,8,9,10]
-const newNums = myNums.map( (num) => num + 10)
+let newNums = myNums.map( (num) => num + 10)
+console.log(newNums);
+
+newNums = myNums.map( (nums) => nums * 10)
+          .map( (nums) => nums + 1)
+          .filter( (nums) => nums >= 40 )
+
 console.log(newNums);
