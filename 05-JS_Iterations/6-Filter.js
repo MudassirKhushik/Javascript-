206aef014b622c8cca5d54af2b996395ebe28f60
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-21 09:19:21 +0000

    Filter methid in JS

--- 05-JS_Iterations/6-Filter.js
+++ 05-JS_Iterations/6-Filter.js
@@ -15,3 +15,43 @@ myNums.forEach( (nums) => {
     }
 })
 console.log(evenNums);
+
+const bookSeriesFromDB = [
+    {
+        title:"Book One", genre:"History", publish:2009, edition:2012
+    },
+    {
+        title:"Book Two", genre:"Fiction", publish:1999, edition:2002
+    },
+    {
+        title:"Book Three", genre:"Non-Fiction", publish:1993, edition:1999
+    },
+    {
+        title:"Book Four", genre:"Science", publish:1990, edition:2001
+    },
+    {
+        title:"Book Five", genre:"Geography", publish:1995, edition:2011
+    },
+    {
+        title:"Book Six", genre:"History", publish:2000, edition:2012
+    },
+    {
+        title:"Book Seven", genre:"Fiction", publish:2002, edition:2010
+    },
+    {
+        title:"Book Eight", genre:"Non-Fuction", publish:2016, edition:2018
+    },
+    {
+        title:"Book Nine", genre:"Science", publish:2020, edition:2022
+    },
+    {
+        title:"Book Ten", genre:"Geography", publish:2005, edition:2010
+    }
+];
+
+let userBooks = bookSeriesFromDB.filter( (book) => book.genre === "Science")
+
+userBooks = bookSeriesFromDB.filter( (book) => {
+    return book.publish >= 200 && book.genre === "History"
+})
+console.log(userBooks);
