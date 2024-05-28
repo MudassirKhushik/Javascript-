4c754cd20a8807337d133be3be9f23953bc31fcf
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-28 10:31:16 +0000

    JS file

---
+++ 07-JS_Projects/1-Color_Changer/script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelector('button')
+const body = document.querySelector('body')
+
+buttons.forEach(function(button){
+    button.addEventListner('click', function(e){
+        if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'black'){
+            body.style.backgroundColor = e.target.id
+        }
+    })
+})
