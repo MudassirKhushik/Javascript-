8d225a9b855d4af6e75a9a345b07e931816d1d81
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-05-28 11:31:04 +0000

    JS file

--- 07-JS_Projects/1-Color_Changer/script.js
+++ 07-JS_Projects/1-Color_Changer/script.js
@@ -1,22 +1,29 @@
-const buttons = document.querySelector('button');
+const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
 buttons.forEach(function(button){
-    button.addEventListner('click', function(e){
-        if(e.target.id === 'green'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'red'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'grey'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'black'){
-            body.style.backgroundColor = e.target.id
-        }
+    button.addEventListener('click', function(e){
+        document.body.style.backgroundColor = e.target.id
     });
 });
+
+// buttons.forEach(function(button){
+//     button.addEventListener('click', function(e){
+        
+//         if(e.target.id === "green"){
+//             body.style.backgroundColor = e.target.id
+//         }
+//         if(e.target.id === "blue"){
+//             body.style.backgroundColor = e.target.id
+//         }
+//         if(e.target.id === "red"){
+//             body.style.backgroundColor = e.target.id
+//         }
+//         if(e.target.id === "grey"){
+//             body.style.backgroundColor = e.target.id
+//         }
+//         if(e.target.id === "black"){
+//             body.style.backgroundColor = e.target.id
+//         }
+//     });
+// });
