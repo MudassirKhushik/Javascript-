20fe14d5155b7e41266a425766563b2daeaede0d
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2024-07-19 06:23:51 +0000

    Comparision of Datatypes

--- 01-JS_Basics/5-ComparisionDatatypes.js
+++ 01-JS_Basics/5-ComparisionDatatypes.js
@@ -10,17 +10,17 @@
 // console.log("2" > 2);
 // console.log("02" > 2);
 
-// console.log(null > 0);
-// console.log(null >= 0);
-// console.log(null == 0);
-// console.log(null < 0);
-// console.log(null <= 0);
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null < 0);
+console.log(null <= 0);
 
-console.log(undefined > 0);
-console.log(undefined >= 0);
-console.log(undefined == 0);
-console.log(undefined < 0);
-console.log(undefined <= 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
 
 // Strickly checks (===) {Checks the numbers as well as their datatypes}
 console.log("2" === 2); // false
