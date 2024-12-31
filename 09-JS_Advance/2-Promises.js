62652877a8014732aef1b68b9ab565fad26177de
Mudassir Khushik <mudassirkhushik@gmail.com>
2024-12-31 06:09:32 -0800

    Promises, Async-Await, Try-Catch, fetch()

--- 09-JS_Advance/2-Promises.js
+++ 09-JS_Advance/2-Promises.js
@@ -69,10 +69,35 @@ const promiseFive = new Promise(function(resolve, reject){
 
 async function ConsumePromiseFive(){
     try {
-        const respose = await promiseFive;
-        console.log(respose);
+        const response = await promiseFive;
+        console.log(response);
     } catch (error) {
         console.log(error);
     }
 }
 ConsumePromiseFive();
+
+// Fetch with Async Await
+async function getAllUsers(){
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getAllUsers();
+
+// Fetch with then, catch and finally
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);    
+})
+.catch((error) => console.log(error)
+)
+.finally(() => console.log("The issue is either resolved or rejected"));
