dfd731f7561953ee8f1cda14836d91671b959e1f
Mudassir Khushik <mudassirkhushik@gmail.com>
2024-12-31 03:05:30 -0800

    Promises, Async-Await, Try-Catch

--- 09-JS_Advance/2-Promises.js
+++ 09-JS_Advance/2-Promises.js
@@ -18,3 +18,61 @@ new Promise((resolve, reject) => {
 }).then(() => {
     console.log("Promise 2 Completed");
 })
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({
+            username : "Mudassir",
+            email : "mudassir@gmail"
+        });
+    }, 1000);
+})
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({
+                username : "Mudassir",
+                email : "mudassir@gmail"
+            })} else{
+            reject("Error: Something went wrong");
+        }
+    }, 1000);
+})
+
+promiseFour.then(function(user){
+    console.log(user);
+    user.username;
+}).then(function(username){
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => {console.log("The issue is either resolved or rejected");})
+
+// Async Await
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+        resolve({
+            coursename : "Javascript",
+            coursecode : "Mudassir9099"
+        })} else{
+                reject("Error: Something went wrong");
+            }
+    }, 1000);
+})
+
+async function ConsumePromiseFive(){
+    try {
+        const respose = await promiseFive;
+        console.log(respose);
+    } catch (error) {
+        console.log(error);
+    }
+}
+ConsumePromiseFive();
